3c5eae0cd3adbb2b41766690108c970333214643
Bikal Thapa <thapa.bikal@hotmail.com>
2019-11-02 12:06:33 -0400

    js file

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -1,16 +1,16 @@
-function add(ths,sno){
-    for (var i=1;i<=5;i++) {
-        var cur=document.getElementById("star"+i)
-        cur.className="fa fa-star"
+function add(ths, sno) {
+    for (var i = 1; i <= 5; i++) {
+        var cur = document.getElementById("star" + i)
+        cur.className = "fa fa-star"
     }
-    
-    for (var i=1;i<=sno;i++) {
-        var cur=document.getElementById("star"+i)
-        if(cur.className=="fa fa-star") {
-            cur.className="fa fa-star checked"
+
+    for (var i = 1; i <= sno; i++) {
+        var cur = document.getElementById("star" + i)
+        if (cur.className == "fa fa-star") {
+            cur.className = "fa fa-star checked"
         }
     }
-    if (sno ===3) {
+    if (sno === 3) {
         $('#feedbackCard').removeClass('bg-danger')
         $('#feedbackCard').removeClass('bg-sucess')
         $('#feedbackCard').addClass('bg-dark')
