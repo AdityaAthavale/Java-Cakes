2ab16b9c645c322550ef953adb267ae52b99e42a
AdityaAthavale <Aditya.Athavale16@gmail.com>
2019-11-07 20:34:58 -0500

    Cleaning and getting everything to work

--- assets/javascript/databaseController.js
+++ assets/javascript/databaseController.js
@@ -11,50 +11,23 @@ var firebaseConfig = {
     measurementId: "G-01CFYFZEY6"
 };
 
+let appointments = new Array()
+
+$(document).ready(function() {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-
     const database = firebase.database();
 
-    let appointments = new Array()
-    // button for addind info
-    $("#submitForm").on("click", function (event) {
-    event.preventDefault();
+    //Get list of set appointments
+    database.ref('/Appointment').on('value', function(snapshot) {
+        appointments = new Array()
+        snapshot.forEach(element => {
+            appointments.push(element.val().appoinment)
+        })
+    })
 
-    // Grab user input
-    let userName = $("#firstName").val().trim();
-    let userPhoneNumber = $("#phoneNumber").val().trim();
-    let userEmail = $("#email").val().trim();
-    let userFeedback = $("#description").val().trim();
 
-    // creating local "temp" object to hold users data
-    const usersFeedback = {
-        name: userName,
-        phoneNumber: userPhoneNumber,
-        email: userEmail,
-        feedback: userFeedback,
-
-    };
-
-    // Uploads user infoto the database
-    database.ref('/Feedback').push(usersFeedback);
-
-    // Clears all of the text-boxes
-    $("#firstName").val("");
-    $("#phoneNumber").val("");
-    $("#email").val("");
-    $("#description").val("");
-
-});
-
-//  Create Firebase event for adding users info to the database  when a user adds an entry
-database.ref('/Feedback').on("child_added", function (childSnapshot) {
-    console.log(childSnapshot.val());
-
-});
-
-// button for appoinment
-$(document).ready(function() {
+    //Add Button listeners
     $("#submitForm-date").on("click", function (event) {
         debugger;
         event.preventDefault();
@@ -80,21 +53,48 @@ $(document).ready(function() {
         $("#appointment").val("");
     
     });
+
+    $("#submitForm").on("click", function (event) {
+        event.preventDefault();
+    
+        // Grab user input
+        let userName = $("#firstName").val().trim();
+        let userPhoneNumber = $("#phoneNumber").val().trim();
+        let userEmail = $("#email").val().trim();
+        let userFeedback = $("#description").val().trim();
+    
+        // creating local "temp" object to hold users data
+        const usersFeedback = {
+            name: userName,
+            phoneNumber: userPhoneNumber,
+            email: userEmail,
+            feedback: userFeedback,
+    
+        };
+    
+        // Uploads user infoto the database
+        database.ref('/Feedback').push(usersFeedback);
+    
+        // Clears all of the text-boxes
+        $("#firstName").val("");
+        $("#phoneNumber").val("");
+        $("#email").val("");
+        $("#description").val("");
+    
+    });
+
+    //  Create Firebase event for adding users info to the database  when a user adds an entry
+    database.ref('/Appointment').on("child_added", function (childSnapshot) {
+        console.log(childSnapshot.val());
+    });
+
+    //  Create Firebase event for adding users info to the database  when a user adds an entry
+    database.ref('/Feedback').on("child_added", function (childSnapshot) {
+        console.log(childSnapshot.val());
+
+    });
+
 })
-
-
-//Get list of set appointments
-database.ref('/Appointment').on('value', function(snapshot) {
-    appointments = new Array()
-    snapshot.forEach(element => {
-        appointments.push(element.val().appoinment)
-    })
-})
-
-//  Create Firebase event for adding users info to the database  when a user adds an entry
-database.ref('/Appointment').on("child_added", function (childSnapshot) {
-    console.log(childSnapshot.val());
-});
 
 
 
--- assets/javascript/receipe.js
+++ assets/javascript/receipe.js
@@ -1,85 +1,80 @@
+// This is the API key. 
+var APIKey = "d5194eec5a7983605d72b23429e0724d";
+let receipeCount = 10
+let APPId = "361b960d"
+
 $(document).ready(function () {
-    // This is the API key. 
-    var APIKey = "d5194eec5a7983605d72b23429e0724d";
-
-    // ***********************
-    function displayCakes(evt) {
-        var queryURL1 =
-            "https://api.edamam.com/search?q=cupcakes&to=100&app_id=361b960d&app_key=d5194eec5a7983605d72b23429e0724d";
-
-        $.ajax({
-            url: queryURL1,
-            method: "GET"
-        }).then(function (response) {
-            let hits = response.hits;
-
-            // function eachOne(){}
-            hits.forEach(element => {
-                let imgPath = element.recipe.image
-                let labelPath = element.recipe.label
-                let sitePath = element.recipe.url
-                $("#happy").append(makeCard(sitePath, labelPath, imgPath))
-            }); // end forEach
-        }); //end ajax call
-    } // end displayCakes()
-
-    // ***********************
-    function displayPies(evt) {
-        let image;
-        // This is our API key. Add your own API key between the ""
-        var APIKey = "d5194eec5a7983605d72b23429e0724d";
-
-        var queryURL2 =
-            "https://api.edamam.com/search?q=pies&to=100&app_id=361b960d&app_key=d5194eec5a7983605d72b23429e0724d";
-
-        // create an AJAX call
-        $.ajax({
-            url: queryURL2,
-            method: "GET"
-        }).then(function (response) {
-            let hits = response.hits;
-
-            // function eachOne(){}
-            hits.forEach(element => {
-                let imgPath = element.recipe.image
-                let labelPath = element.recipe.label
-                let sitePath = element.recipe.url
-                $("#happier").append(makeCard(sitePath, labelPath, imgPath))
-            }); // end forEach
-        }); //end ajax call
-    } // end displayPastry()
-
-
-    // ***********************
-    function displayCookies(evt) {
-        let image;
-        // This is our API key. Add your own API key between the ""
-        var APIKey = "d5194eec5a7983605d72b23429e0724d";
-
-        var queryURL3 =
-            "https://api.edamam.com/search?q=cookies&to=100&app_id=361b960d&app_key=d5194eec5a7983605d72b23429e0724d";
-
-        // create an AJAX call
-        $.ajax({
-            url: queryURL3,
-            method: "GET"
-        }).then(function (response) {
-            let hits = response.hits;
-
-            // function eachOne(){}
-            hits.forEach(element => {
-                let imgPath = element.recipe.image
-                let labelPath = element.recipe.label
-                let sitePath = element.recipe.url
-                $("#happiest").append(makeCard(sitePath, labelPath, imgPath))
-            }); // end forEach
-        }); //end ajax call
-    } // end displayPastry()
-
     displayCakes();
     displayPies();
     displayCookies();
 });
+ // ***********************
+ function displayPies(evt) {
+    let image;
+    var queryURL2 =
+        "https://api.edamam.com/search?q=pies&to=" + receipeCount + "&app_id="+ APPId + "&app_key=" + APIKey;
+
+    // create an AJAX call
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    }).then(function (response) {
+        let hits = response.hits;
+
+        // function eachOne(){}
+        hits.forEach(element => {
+            let imgPath = element.recipe.image
+            let labelPath = element.recipe.label
+            let sitePath = element.recipe.url
+            $("#happier").append(makeCard(sitePath, labelPath, imgPath))
+        }); // end forEach
+    }); //end ajax call
+} // end displayPastry()
+
+
+// ***********************
+function displayCookies(evt) {
+    let image;
+
+    var queryURL3 =
+        "https://api.edamam.com/search?q=cookies&to=" + receipeCount + "&app_id="+ APPId + "&app_key=" + APIKey;
+
+    // create an AJAX call
+    $.ajax({
+        url: queryURL3,
+        method: "GET"
+    }).then(function (response) {
+        let hits = response.hits;
+
+        // function eachOne(){}
+        hits.forEach(element => {
+            let imgPath = element.recipe.image
+            let labelPath = element.recipe.label
+            let sitePath = element.recipe.url
+            $("#happiest").append(makeCard(sitePath, labelPath, imgPath))
+        }); // end forEach
+    }); //end ajax call
+} // end displayPastry()
+// ***********************
+function displayCakes(evt) {
+    var queryURL1 =
+        "https://api.edamam.com/search?q=cupcakes&to=" + receipeCount + "&app_id="+ APPId + "&app_key=" + APIKey;
+
+    $.ajax({
+        url: queryURL1,
+        method: "GET"
+    }).then(function (response) {
+        let hits = response.hits;
+
+        // function eachOne(){}
+        hits.forEach(element => {
+            let imgPath = element.recipe.image
+            let labelPath = element.recipe.label
+            let sitePath = element.recipe.url
+            $("#happy").append(makeCard(sitePath, labelPath, imgPath))
+        }); // end forEach
+    }); //end ajax call
+} // end displayCakes()
 
 function makeCard(link, label, image) {
     let cardDiv = $('<div>')
